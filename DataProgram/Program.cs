4fa6b3ef2a89e0bd269a5d03162ae12c391cbe1e
rejoshi <rejoshi@ICS-LT-G2X1HK3>
2026-08-25 21:57:50 +0545

    Add project files.

---
+++ DataProgram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class program
+{
+    static void Main(string[] args)
+    {
+       int taskId = 1;
+        string  description = "This is a sample task description.";
+        bool isCompleted = false;
+        double estimatedHours = 5.5;
+        DateTime dueDate = new DateTime(2024, 12, 31);
+
+
+        Console.WriteLine("Task Datails");
+        Console.WriteLine("taskId: " + taskId);
+        Console.WriteLine("description: " + description);
+        Console.WriteLine("isCompleted: " + isCompleted);
+        Console.WriteLine("estimatedHours: " + estimatedHours);
+        Console.WriteLine("dueDate: " + dueDate.ToShortDateString());
+    }
+}
